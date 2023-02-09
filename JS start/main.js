e98127dee1844a8668e0f99a73452ebc5d75dc3b
Artem <tihvin2003@gmail.com>
2023-02-09 19:26:46 +0300

    all fine

---
+++ JS start/main.js
@@ -0,0 +1,65 @@
+// const objectA = {
+//     a: 10,
+//     b: 20
+// }
+
+// const test = objectA
+// objectA.a = 30
+// objectA.c = 'abc'
+// console.log(objectA)
+
+
+// function a() {
+//     console.log('Hey there')
+// }
+
+// a()
+
+// const myCity = {
+//     city: 'New York',
+//     popular: true,
+//     country: 'USA',
+// }
+
+// delete myCity.country
+// console.log(myCity)
+
+// const name = 'Artem'
+// const postsQty = 19
+
+// const userProfile = {
+//     name: name,
+//     postsQty: postsQty,
+//     hasSignedAgreement: false,
+// }
+
+// console.log(userProfile)
+
+// const person = {
+//     name:'bob',
+//     age: 25,
+// }
+
+// const person2 = Object.assign({}, person)
+
+// person2.age = 26
+
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person2.age)
+// console.log(person2.name)
+
+// function myFn(a, b) {
+//     let c
+//     a = a ** 5
+//     c = a * b
+//     return c
+// }
+
+// console.log(myFn(7, 3))
+
+function myName() {
+    console.log('Artem')
+}
+
+setTimeout(myName, 1000)
