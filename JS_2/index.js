88a103cdea5b3d068b9270c6f3f5cf525cd39aa8
Artem <tihvin2003@gmail.com>
2023-02-10 23:51:02 +0300

    js2

---
+++ JS_2/index.js
@@ -0,0 +1,47 @@
+// const a = 5 
+
+// function myFn() {
+//     function innerFn() {
+//         console.log(a)
+//     }
+//     innerFn()
+// }
+
+// myFn()
+
+// let a
+// let b
+
+// function myFn() {
+//     let b
+//     a = 1
+//     b = 2
+//     console.log(a, b)
+// }
+
+// myFn()
+
+// console.log(a)
+// console.log(b)
+
+// console.log(!10)
+// console.log(!0)
+// console.log(!'abc')
+// console.log(!'')
+// console.log(!true)
+// console.log(!undefined)
+
+// console.log('artem'||'default')
+// console.log(''||'default')
+// console.log(''||'')
+
+// const button = {
+//     width: 200,
+//     text: 'buy'
+// }
+
+// const redButton = {
+//     ...button, color: 'red'
+// }
+
+// console.table(redButton)
