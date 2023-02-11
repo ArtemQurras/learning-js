c6c9e13aa89e236d2c7c3e1037a73e3dc53d45fe
Artem <tihvin2003@gmail.com>
2023-02-11 23:07:58 +0300

    'add3'

---
+++ js_3/index.js
@@ -0,0 +1,65 @@
+// const buttonInfo = {
+//     text: 'buy'
+// }
+
+// const buttonStyle = {
+//     color: 'yellow',
+//     width: 200,
+//     height: 300,
+// }
+
+// const button = {
+//     ...buttonInfo,
+//     ...buttonStyle,
+// }
+
+// console.table(button)
+
+// const hello = 'hello'
+// const world = 'world'
+
+// const great = `${hello} ${world}`
+
+// console.log(great)
+
+// const myName = 'Артём'
+// const myCity = 'Санкт-Петербург'
+// const text = `Всем привет! Меня зовут ${myName} и я живу в городе ${myCity}`
+// console.log(text)
+
+// function multself(value, mult = 1) {
+//     return value * mult
+// }
+// console.log(10,2)
+// console.log(5)
+
+// const newPost = (post, addeadAt = Date()) => ({
+//     ...post,
+//     addeadAt,
+// })
+
+// const firstPost = {
+//     id: 1,
+//     author: 'Artem'
+// }
+
+// console.log(newPost(firstPost))
+
+
+
+// const fnWithError = () => {
+//     throw new Error('Some error')
+// }
+
+// try {
+//     fnWithError()
+// }   catch (error) {
+//     console.error(error) //! Показывает всю ошибку вместе с текстом
+//     console.log(error.message) //! Показывает только текст Some error
+// }
+    
+
+// console.log('Continue...')
+
+
+
