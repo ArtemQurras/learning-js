b19ba58528c352c49b566f743c6d9d680a683155
Artem <tihvin2003@gmail.com>
2023-02-14 23:35:58 +0300

    'js6'

---
+++ js5/index.js
@@ -0,0 +1,43 @@
+// const myArray = [1, 2, 3]
+// console.log(myArray)
+
+// const myArray2 = new Array(1, 2, 3)
+// console.log(myArray2)
+
+// const myArray3 = new Array(1, true, 'artem')
+// console.log(myArray2)
+
+// myArray4 = myArray3
+// console.log(myArray === myArray2)
+// console.log(myArray3 === myArray4)
+
+// const aray = [1, true, 'artem']
+// console.log(aray[0])
+// console.log(aray[1])
+// console.log(aray[2])
+// console.log(aray.length)
+
+// const myArray = [1,2,3,4]
+// console.log(myArray)
+// console.log(myArray.length)
+
+// myArray[2] = 'abc'
+// console.log(myArray)
+// console.log(myArray[2])
+
+// myArray[4] = true
+// console.log(myArray)
+// console.log(myArray.length)
+
+const myArray = [1,2,3]
+console.log(myArray)
+
+myArray.push(4)
+
+myArray.push('abc')
+
+myArray.push(true)
+
+console.log(myArray)
+
+console.log(myArray.length)
