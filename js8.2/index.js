61b07f87ca4cfdaf1ace56096ea3f229e4aa1d77
Artem <tihvin2003@gmail.com>
2023-02-20 21:20:23 +0300

    'js8.2'

---
+++ js8.2/index.js
@@ -0,0 +1,70 @@
+// const myArray = [1, 2, 3, 15, 45]
+// console.log(myArray)
+
+// myArray.forEach(el => console.log(el * 2))
+// console.log(myArray)
+//! ------------------------------------------------------
+// const myArray = [1, 2, 3, 15, 45]
+// console.log(myArray)
+
+// const newArray = myArray.map(el => el * 3)
+// console.log(myArray)
+// console.log(newArray)
+//! ------------------------------------------------------
+
+// const myArray = [1, 2, 3, 15, 45]
+// console.log(myArray)
+
+// const newArray = myArray.map((el) => {
+//     return el * 3
+// })
+// console.log(myArray)
+// console.log(newArray)
+//! ------------------------------------------------------
+
+// const myArray = [1, 2, 3, 15, 45]
+// console.log(myArray)
+
+// const newArray = myArray.map(function (el) {
+//     return el * 3
+// })
+// console.log(myArray)
+// console.log(newArray)
+//! ------------------------------------------------------
+
+// const userProfile = {
+//     name: 'Artem',
+//     CommentsQty: 23,
+//     hasSignedAgreement: false,
+// }
+
+// const {name, CommentsQty} = userProfile
+// const {hasSignedAgreement} = userProfile
+
+// console.log(name)
+// console.log(hasSignedAgreement)
+// console.log(CommentsQty)
+
+//! ------------------------------------------------------
+
+// const fruits = ['Apple', 'Banana']
+// const [fruitOne, fruitTwo] = fruits
+
+// console.log(fruitOne)
+// console.log(fruitTwo)
+//! ------------------------------------------------------
+
+// const userProfile = {
+//     name: 'Artem',
+//     commentsQty: 19,
+//     hasSignedAgreement: false
+// }
+
+// const userInfo = ({ name , commentsQty}) => {
+//     if (!commentsQty) {
+//         return `User ${name} has no comments`
+//     }
+//     return `User ${name} has ${commentsQty} comments`
+// }
+// console.log(userInfo(userProfile)) 
+//! ------------------------------------------------------
